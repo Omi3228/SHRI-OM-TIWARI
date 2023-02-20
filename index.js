f6a155a0af36f80192cf4b5951d033a5b69a6062
SHRI OM TIWARI <shriomtiwari3228@gmail.com>
2023-02-20 12:17:29 -0800

    MY PORTFOLIO

---
+++ index.js
@@ -0,0 +1,39 @@
+// console.log("This Is For Loop in JS")
+
+// let i = 0;
+// for(i=0; i<3; i++)
+// {
+//     console.log(i);
+// }
+
+
+//let friends = ["SHRI","ROHAN", "RITIK", "MUKESH"];
+// for (let index = 0; index < friends.length; index++) {
+//      element = friends[index];
+//      console.log("HELLO,  " + friends[index]);
+//       }
+
+
+// alert("Harry bhai Uploading soon the Crazy JS tutorial");
+
+// let cwh = confirm("Harry bhai jaldi kro mere bhai i m waiting ") ;
+// console. log (cwh) ;
+
+// if(cwh) {
+// console. log ("Are You uploading the Tutorial🙂 " ) ;
+// } 
+// else{
+// console. log ("I m waiting some time for the crazy JS Tutorial 🥰" ) ;
+// } 
+
+
+ alert("Welcome! For Better Best Experience Open this Website on PC/LAPTOP and In the Smartphone/Tablet using Desktop Site! || Website is Under Development!!!!!!!");
+
+
+
+/*let text;
+if (confirm("Press a button!") == true) {
+  text = "You pressed OK!";
+} else {
+  text = "You canceled!";
+}*/
